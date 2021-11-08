10c17d1eebee77956a45022b1f0a64590a0a2917
Cola <remondn@spacewalk.tech>
2021-11-08 04:09:01 -0500

    :white_check_mark: Add tests for transforms

---
+++ tests/test_transforms.py
@@ -0,0 +1,48 @@
+from math import isclose
+
+import pytest
+from shapely.geometry import box, Point, LineString
+
+from cartesius.transforms import NormalizePositionTransform, NormalizeScaleTransform
+
+
+@pytest.mark.parametrize("p", [
+    box(1, 1, 10, 10),
+    box(1, -10, 10, -1),
+    box(-10, 1, -1, 10),
+    box(-10, -10, -1, -1),
+    box(1, -10, 10, 10),
+    box(-10, 1, 10, 10),
+    box(-10, -10, 10, 10),
+])
+def test_norm_pos(p):
+    transfo = NormalizePositionTransform()
+
+    result = transfo(p)
+
+    min_x, min_y, *_ = result.bounds
+    assert min_x == 0 and min_y == 0
+    assert result.area == p.area
+
+
+@pytest.mark.parametrize("p", [
+    box(1, 1, 10, 10),
+    box(1, 1, 10, 5),
+    box(1, 1, 5, 10),
+    Point((1, 1)),
+    LineString([(1, 1), (10, 10)]),
+    box(1, 1, 1.2, 1.1),
+])
+def test_norm_scale(p):
+    transfo = NormalizeScaleTransform()
+    min_x, min_y, max_x, max_y = p.bounds
+    scale_size = max([max_x - min_x, max_y - min_y])
+
+    result = transfo(p)
+
+    p_min_x, p_min_y, *_ = p.bounds
+    r_min_x, r_min_y, *_ = result.bounds
+    assert p_min_x == r_min_x and p_min_y == r_min_y
+    if scale_size !=0 :
+        scale_ratio = 1 / scale_size
+        assert isclose(result.area, p.area * scale_ratio ** 2)

b7e2c8923df1a5ae36b3a9fff2a3052de873075b
Cola <remondn@spacewalk.tech>
2021-11-09 09:23:02 -0500

    :package: Fix package data

--- setup.py
+++ setup.py
@@ -13,23 +13,21 @@ extras_require = {
 extras_require["all"] = sum(extras_require.values(), [])
 extras_require["dev"] = extras_require["docs"] + extras_require["tests"] + extras_require["lint"]
 
-setuptools.setup(
-    name="cartesius",
-    version="0.1.dev0",
-    author="Nicolas REMOND",
-    author_email="remondn@spacewalk.tech",
-    description="Benchmark & Pretraining for Cartesian coordinates feature extraction",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/TeamSPWK/cartesius",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3.6",
-        "Operating System :: OS Independent",
-    ],
-    python_requires=">=3.6",
-    install_requires=reqs,
-    extras_require=extras_require,
-    entry_points={"console_scripts": ["cartesius=cartesius.train:main"]},
-    data_files=[("", ["config/*.yaml", "data/*.json"])],
-)
+setuptools.setup(name="cartesius",
+                 version="0.1.dev0",
+                 author="Nicolas REMOND",
+                 author_email="remondn@spacewalk.tech",
+                 description="Benchmark & Pretraining for Cartesian coordinates feature extraction",
+                 long_description=long_description,
+                 long_description_content_type="text/markdown",
+                 url="https://github.com/TeamSPWK/cartesius",
+                 packages=setuptools.find_packages(),
+                 classifiers=[
+                     "Programming Language :: Python :: 3.6",
+                     "Operating System :: OS Independent",
+                 ],
+                 python_requires=">=3.6",
+                 install_requires=reqs,
+                 extras_require=extras_require,
+                 entry_points={"console_scripts": ["cartesius=cartesius.train:main"]},
+                 package_data={"cartesius": ["config/*.yaml", "data/*.json"]})

8b8e1fa0d61387208c3cedfe587919797b31e9ce
Cola <remondn@spacewalk.tech>
2021-11-15 18:38:15 -0500

    :white_check_mark: Add test for graph tokenizer

--- tests/test_tokenizers.py
+++ tests/test_tokenizers.py
@@ -78,7 +78,7 @@ def test_graph_tokenizer_single_not_polygon(p):
             [0, 1],
         ]
     else:
-        assert result["edge_index"].tolist() == [[0, 0]]
+        assert result["edge_index"].tolist() == [[0], [0]]
 
 
 def test_graph_tokenizer_batched_polygons():
@@ -94,4 +94,21 @@ def test_graph_tokenizer_batched_polygons():
     assert result["edge_index"].tolist() == [
         [1, 0, 2, 1, 0, 2, 4, 3, 5, 4, 6, 5, 3, 6],
         [0, 1, 1, 2, 2, 0, 3, 4, 4, 5, 5, 6, 6, 3],
+    ]
+
+
+def test_graph_tokenizer_batched_all_types():
+    tokenizer = GraphTokenizer()
+    p = [
+        Point(0.5, 0.5),
+        Polygon([(0, 0), (0, 1), (1, 0), (0, 0)]),
+        LineString([(0, 0), (1, 1)]),
+    ]
+
+    result = tokenizer(p)
+
+    assert result["x"].tolist() == [list(c) for c in p[0].coords] + [list(c) for c in p[1].boundary.coords[:-1]] + [list(c) for c in p[2].coords]
+    assert result["edge_index"].tolist() == [
+        [0, 2, 1, 3, 2, 1, 3, 5, 4],
+        [0, 1, 2, 2, 3, 3, 1, 4, 5],
     ]
